14ba99e3a8a085e2ded2f310a38e63683525fa02
Joseph Skinner <jskinner@spidadesign.com>
2014-07-22 10:29:23 -0400

    Mobile!!!
    - Most of iPhone responsive is done.
    - Added custom google maps to contact page
    - General bug fixes and minor css changes

--- wp-content/themes/talara/footer.php
+++ wp-content/themes/talara/footer.php
@@ -14,6 +14,7 @@
 	<script src="<?php bloginfo('template_directory'); ?>/bower_components/twbs-bootstrap-sass/assets/javascripts/bootstrap/collapse.js"></script>
 	<script src="<?php bloginfo('template_directory'); ?>/bower_components/twbs-bootstrap-sass/assets/javascripts/bootstrap/transition.js"></script>
 	<script src="<?php bloginfo('template_directory'); ?>/bower_components/slick-carousel/slick/slick.min.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKGY4G6u_ZpjZPizMVu6E__tqxXay_Nfc"> </script>
 	<script type="text/javascript">
 	$(document).ready(function(){
 			$('.header-slider').slick({
@@ -38,7 +39,22 @@
 		      kitId: 'xlw8cob',
 		      scriptTimeout: 3000
 		    },
-		    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+		    h=d.documentElement,t=setTimeout(function(){
+		    	h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";
+		    },
+		    config.scriptTimeout),tk=d.createElement("script"),
+		    f=false,s=d.getElementsByTagName("script")[0],a;
+		    h.className+=" wf-loading";
+		    tk.src='//use.typekit.net/'+config.kitId+'.js';
+		    tk.async=true;
+		    tk.onload=tk.onreadystatechange=function(){
+		    	a=this.readyState;
+		    	if(f||a&&a!="complete"&&a!="loaded")
+		    		return;
+		    	f=true;
+		    	clearTimeout(t);
+		    	try{Typekit.load(config)}catch(e){}};
+		    	s.parentNode.insertBefore(tk,s)
 		  })(document);
 		  $("button.more").click(function(){
 				if($(this).hasClass('alive')){
@@ -54,7 +70,79 @@
 					});
 				}
 			});
+			function initialize() {
+				var styles = [
+					{
+				      stylers: [
+				        //{ hue: "#b5b1a9" },
+				        { saturation: -100 },
+				        { lightness: -10 },
+				        { gamma: 1.00 }
+				      ]
+				    },{
+				      featureType: "road",
+				      elementType: "geometry",
+				      stylers: [
+				        { lightness: 100 },
+				        { visibility: "simplified" }
+				      ]
+				    },{
+				      featureType: "road",
+				      elementType: "labels",
+				      stylers: [
+				        { visibility: "off" }
+				      ]
+				    },{
+					    featureType: "poi.business",
+					    elementType: "labels",
+					    stylers: [
+					      { visibility: "off" }
+					    ]
+					  },
+				  ];
+				var styledMap = new google.maps.StyledMapType(styles,
+    				{name: "Styled Map"});
 
+				var mapOptions = {
+		        	center: new google.maps.LatLng(40.7554746, -73.970512),
+		          	zoom: 15,
+		          	mapTypeControlOptions: {
+      				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    				}
+		        };
+		        var houstonOptions = {
+		        	center: new google.maps.LatLng(29.756016, -95.362459),
+		          	zoom: 15,
+		          	mapTypeControlOptions: {
+      				mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'houston_style']
+    				}
+		        };
+
+		        var map = new google.maps.Map(document.getElementById("new-york"),
+		        								mapOptions);
+		        var houston = new google.maps.Map(document.getElementById("houston"),
+		        								houstonOptions);
+
+		        var image = "<?php bloginfo('template_directory'); ?>/assets/images/marker.png";
+		        var nyLatLng= new google.maps.LatLng(40.7554746, -73.970512);
+		        var houstonLatLng= new google.maps.LatLng(29.756016, -95.362459);
+		        var talMarker = new google.maps.Marker({
+     				position: nyLatLng,
+      				map: map,
+      				icon: image
+  				});
+  				var houstonMarker = new google.maps.Marker({
+     				position: houstonLatLng,
+      				map: houston,
+      				icon: image
+  				});
+  				map.mapTypes.set('map_style', styledMap);
+  				map.setMapTypeId('map_style');
+
+  				houston.mapTypes.set('houston_style', styledMap);
+  				houston.setMapTypeId('houston_style');
+		      }
+		      google.maps.event.addDomListener(window, 'load', initialize);
 		</script>
 	</body>
 </html>
--- wp-content/themes/talara/page-templates/contact.php
+++ wp-content/themes/talara/page-templates/contact.php
@@ -13,20 +13,22 @@
 		<h3>New York Office</h3>
 		<span>805 Third Ave, Floor 20</span>
 		<span>New York, NY 10022</span>
-		<img src="<?php bloginfo('template_directory'); ?>/assets/images/ny-map.png" class="img-responsive">
+		<div id="new-york" class="map-canvas"></div>
 	</div>
 	<div class="col-md-6">
 		<h2>Talara Capital Management</h2>
 		<h3>Houston Office</h3>
 		<span>1221 McKinney Street, Floor 21</span>
 		<span>Houston, TX 77010</span>
-		<img src="<?php bloginfo('template_directory'); ?>/assets/images/tx-map.png" class="img-responsive">
+		<div id="houston" class="map-canvas"></div>
 	</div>
-	<div class="joe">
-		<h2>Joseph Tremblay</h2>
-		<h3>Director of Marketing</h3>
-		<span>646-396-6045</span>
-		<span>jtrembaly@talaracapital.com</span>
+	<div class="col-md-4">
+		<div class="joe">
+			<h2>Joseph Tremblay</h2>
+			<h3>Director of Marketing</h3>
+			<span>646-396-6045</span>
+			<span>jtrembaly@talaracapital.com</span>
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
