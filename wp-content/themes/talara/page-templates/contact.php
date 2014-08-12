9a0e59396a424c007633d7c471398a53425b776a
Joseph Skinner <jskinner@spidadesign.com>
2014-08-12 15:17:12 -0400

    Client Changes
    - Added post types order, so now client can drag and drop
    Contact Page:
    - Removed Talara Capital management from above address
    - Removed Joseph Tremblay’s info
    - Added “for more info…”
    - Made map marker smaller
    
    Our Company:
    - First Letter of each paragraph should be a little smaller

--- wp-content/themes/talara/page-templates/contact.php
+++ wp-content/themes/talara/page-templates/contact.php
@@ -9,26 +9,23 @@
 </div>
 <div class="container">
 	<div class="col-md-6">
-		<h2>Talara Capital Management</h2>
 		<h3>New York Office</h3>
 		<span>805 Third Ave, Floor 20</span>
 		<span>New York, NY 10022</span>
 		<div id="new_york" class="map-canvas"></div>
 	</div>
 	<div class="col-md-6">
-		<h2>Talara Capital Management</h2>
 		<h3>Houston Office</h3>
 		<span>1221 McKinney Street, Floor 21</span>
 		<span>Houston, TX 77010</span>
 		<div id="houston" class="map-canvas"></div>
 	</div>
 	<div class="col-md-4">
-		<div class="joe">
-			<h2>Joseph Tremblay</h2>
-			<h3>Director of Marketing</h3>
-			<span>646-396-6045</span>
-			<span>jtremblay@talaracapital.com</span>
-		</div>
+		<span class="more-info">For more information on Talara Capital Management, please contact us at 
+			<span class="red">(646)396-6040</span> 
+			or email us at 
+			<a href="mailto:info@talaracapital.com">info@talaracapital.com</a>
+		</span>
 	</div>
 </div>
 <?php get_footer(); ?>
